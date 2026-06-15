ecb80478df686f8a71fc153a8b6f88dca4b1131c
MigueSaint <amg.sosa@yavirac.edu.ec>
2026-06-15 10:53:41 -0500

    hora actual

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return {"status": "success", "message": "¡Hola Mundo desde Flask, Docker y GitHub Actions!"}
+def get_time():
+    # Obtenemos la fecha y hora actual del sistema
+    now = datetime.now()
+    
+    return {
+        "status": "success",
+        "message": "Servidor Flask activo",
+        "current_time": now.strftime("%Y-%m-%d %H:%M:%S")  # Formato: Año-Mes-Día Hora:Minutos:Segundos
+    }
 
 if __name__ == '__main__':
-    # El host 0.0.0.0 es obligatorio para que Docker pueda redirigir el puerto
     app.run(host='0.0.0.0', port=5000)
